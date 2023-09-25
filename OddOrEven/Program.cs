b7dc0d7a05bc1011fda2a09cbac2faf69f8d9c90
Oliver Juss <Oliver@Triins-MacBook-Pro-2.local>
2023-09-25 12:17:29 +0300

    add  Week 3 code

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,32 @@
+﻿while (true)
+{
+
+    Console.WriteLine("Eneter a number");
+    string userInput = Console.ReadLine();
+    if (userInput == "done")
+    {
+        break;
+    }
+    int num = 0;
+    bool isNum = Int32.TryParse(userInput, out num);
+    Console.WriteLine(userInput);
+    int even = num % 2; 
+    if (isNum == false)
+    {
+        continue;
+    }
+    else 
+    {
+        if (even == 0)
+        {
+            Console.WriteLine($"This number: {userInput} is even");
+            continue;
+        }
+        else
+        {
+            Console.WriteLine($"This number: {userInput} is odd");
+            continue;
+        }
+    }
+ 
+}
